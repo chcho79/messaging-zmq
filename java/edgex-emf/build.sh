7253d3ffab50bdb8ead96e4ebe1617f4684c9311
bg.chun <bg.chun@samsung.com>
2017-11-16 14:45:33 +0900

    Update edgex version to 0.2.0
    Signed-off-by: bg.chun <bg.chun@samsung.com>

--- java/edgex-emf/build.sh
+++ java/edgex-emf/build.sh
@@ -6,16 +6,16 @@ cd ../../dependencys
 DEP_ROOT=$(pwd)
 
 #Clone and install core-domain service
-cd $DEP_ROOT
-git clone https://github.com/edgexfoundry/core-domain.git
-cd core-domain
-mvn install -Dmaven.test.skip=true -U
+#cd $DEP_ROOT
+#git clone https://github.com/edgexfoundry/core-domain.git
+#cd core-domain
+#mvn install -Dmaven.test.skip=true -U
 
 #Clone and install support-logging-client service
-cd $DEP_ROOT
-git clone https://github.com/edgexfoundry/support-logging-client.git
-cd support-logging-client
-mvn clean install -U
+#cd $DEP_ROOT
+#git clone https://github.com/edgexfoundry/support-logging-client.git
+#cd support-logging-client
+#mvn clean install -U
 
 #start package device-service
 cd $PROJECT_ROOT
